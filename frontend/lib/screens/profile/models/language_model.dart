28747e2d8790f1cd4b0575d17f0f02ac71349e95
unknown <earthen505@gmail.com>
2026-04-09 21:40:17 +0700

    Update the Model (language_model.dart) Add the code string so the system knows which dictionary to pull from

--- frontend/lib/screens/profile/models/language_model.dart
+++ frontend/lib/screens/profile/models/language_model.dart
@@ -1,23 +1,19 @@
 class Language {
+  final String code;
   final String flag;
   final String name;
 
-  const Language({
-    required this.flag,
-    required this.name,
-  });
+  const Language({required this.code, required this.flag, required this.name});
 
   factory Language.fromJson(Map<String, dynamic> json) {
     return Language(
+      code: json['code'] ?? 'en',
       flag: json['flag'],
       name: json['name'],
     );
   }
 
   Map<String, dynamic> toJson() {
-    return {
-      'flag': flag,
-      'name': name,
-    };
+    return {'code': code, 'flag': flag, 'name': name};
   }
 }
