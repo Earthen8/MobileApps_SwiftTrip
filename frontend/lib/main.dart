0da6d2f0847deed778a2d8f928a1ea57d2505010
unknown <earthen505@gmail.com>
2026-04-13 10:02:28 +0700

    Locking App Orientation

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:swifttrip_frontend/providers/wishlist_provider.dart';
 import 'package:swifttrip_frontend/providers/cart_provider.dart';
 import 'package:swifttrip_frontend/providers/language_provider.dart';
 import 'package:swifttrip_frontend/screens/splash/splash_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
   runApp(
     MultiProvider(
       providers: [
